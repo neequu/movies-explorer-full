4844b299e90d79a3b4c37eedecbc7bd64648b41d
nikita stepanov <nist1701@gmail.com>
2023-08-26 19:29:35 +0300

    fixed after review

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ import { OK_STATUS } from '../utils/constants.js';
 
 import BadRequestError from '../errors/bad-request.js';
 import NotFoundError from '../errors/not-found.js';
+import ConflictError from '../errors/confilct.js';
 
 const updateUser = async (req, res, next, data) => {
   const owner = req.user._id;
@@ -18,23 +19,34 @@ const updateUser = async (req, res, next, data) => {
 
     return res.status(OK_STATUS).json(user);
   } catch (err) {
-    if (err instanceof mongoose.Error.ValidationError) return next(new BadRequestError('bad data'));
+    if (err instanceof mongoose.Error.ValidationError) { return next(new BadRequestError('bad data')); }
     return next(err);
   }
 };
 
-export const updateProfile = (req, res, next) => {
+export const updateProfile = async (req, res, next) => {
   const { name, email } = req.body;
-  return updateUser(req, res, next, { name, email });
+
+  try {
+    const userWithEmail = await User.findOne({ email });
+    if (userWithEmail) {
+      throw new ConflictError('email already exists');
+    }
+    return updateUser(req, res, next, { name, email });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 export const getCurrentUser = async (req, res, next) => {
   const userId = req.user._id;
   try {
-    const user = await User.findById(userId).orFail(() => { throw new NotFoundError('user not found'); });
+    const user = await User.findById(userId).orFail(() => {
+      throw new NotFoundError('user not found');
+    });
     return res.status(OK_STATUS).json(user);
   } catch (err) {
-    if (err instanceof mongoose.Error.CastError) return next(new BadRequestError('bad data'));
+    if (err instanceof mongoose.Error.CastError) { return next(new BadRequestError('bad data')); }
     return next(err);
   }
 };
